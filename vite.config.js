f6e775c051187af79e8d86beba04e7a94d971770
wmmeg <wmmegal@gmail.com>
2023-01-17 11:34:27 +0200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,42 @@ import {resolve} from 'path';
 
 export default {
     root: resolve(__dirname, 'src'),
+    base: '',
+    build: {
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                app: resolve(__dirname, 'src', 'index.html'),
+                main: resolve(__dirname, 'src', 'main.html'),
+                text: resolve(__dirname, 'src', 'text.html'),
+                certs: resolve(__dirname, 'src', 'certs.html'),
+                catalog: resolve(__dirname, 'src', 'catalog.html'),
+                catalog2: resolve(__dirname, 'src', 'catalog2.html'),
+                catalog3: resolve(__dirname, 'src', 'catalog3.html'),
+                novosti: resolve(__dirname, 'src', 'novosti.html'),
+                partners: resolve(__dirname, 'src', 'partners.html'),
+                contacts: resolve(__dirname, 'src', 'contacts.html'),
+            },
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.')[1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img';
+                    }
+                    if (/woff|woff2|ttf/i.test(extType)) {
+                        extType = 'fonts';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+            },
+        },
+    },
     resolve: {
         alias: {
             '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+            '@': resolve(__dirname, 'src'),
         }
     },
     plugins: [
